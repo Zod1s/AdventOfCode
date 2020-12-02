5fcfa50cc1e737f5f80fb3e4d5f70593046e7ba6
Zod1s <lorefrance15@gmail.com>
2020-12-02 16:14:52 +0100

    First day 2019

--- AdventOfCode2019/Day1/Day1.hs
+++ AdventOfCode2019/Day1/Day1.hs
@@ -3,5 +3,16 @@ import System.IO
 main :: IO()
 main = do
   handle <- openFile "input.txt" ReadMode
-  contents <- getContents handle
-  let text = lines contents
+  contents <- hGetContents handle
+  print $ part2 (lines contents)
+
+part1 :: [String] -> Int
+part1 txt = sum $ map (\x -> (div (read x) 3) - 2) txt
+
+part2 :: [String] -> Int
+part2 txt = sum $ map f masses
+  where masses = map (\x -> (div (read x) 3) - 2) txt
+        f mass = if fuel <= 0
+                 then mass
+                 else mass + f fuel
+          where fuel = (div mass 3) - 2
---
+++ AdventOfCode2019/Day1/Day1.hs~
@@ -0,0 +1,7 @@
+import System.IO
+
+main :: IO()
+main = do
+  handle <- openFile "input.txt" ReadMode
+  contents <- getContents handle
+  let text = lines contents
