f5f8c2a385b075dccbd19dc307a0a2390dddb30c
Zod1s <75039689+Zod1s@users.noreply.github.com>
2020-12-02 09:40:36 +0100

    Update Adv1pt1.hs

--- Day1/Adv1pt1.hs
+++ Day1/Adv1pt1.hs
@@ -7,10 +7,17 @@ main = do
   contents <- hGetContents handle
   let nums = map f (lines contents)
       f n = read n :: Int
-  print $ findNums nums
+  print $ findNums2 nums
 
-findNums :: [Int] -> Int
-findNums ls = foldr foldrFunc 0 [[x, y, z] | x <- ls, y <- ls, z <- ls]
+
+findNums2 :: [Int] -> Int
+findNums2 ls = foldr foldrFunc 0 [[x, y] | x <- ls, y <- ls]
+  where foldrFunc x@(y:z:_) acc = if sum x == 2020
+                                    then y*z
+                                    else acc
+
+findNums3 :: [Int] -> Int
+findNums3 ls = foldr foldrFunc 0 [[x, y, z] | x <- ls, y <- ls, z <- ls]
   where foldrFunc x@(y:z:w:_) acc = if sum x == 2020
                                     then y*z*w
                                     else acc
