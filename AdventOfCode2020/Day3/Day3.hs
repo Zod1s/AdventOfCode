a97360f7333b5ef9b642ce10221e9072390eb669
Zod1s <lorefrance15@gmail.com>
2020-12-03 18:20:22 +0100

    Day 3

---
+++ AdventOfCode2020/Day3/Day3.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BangPatterns #-}
+import System.IO
+
+main :: IO()
+main = do
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  let text = lines contents
+  print $ part2 text
+
+part1 :: [String] -> Int
+part1 = slopeChecker 3 1
+
+slopeChecker :: Int -> Int -> [String] -> Int
+slopeChecker r d strl = go 0 0 strl
+  where go !i acc [] = acc
+        go !i acc (x:xs) = if (c == '#')
+                           then go (i + 1) (acc + 1) xs
+                           else go (i + 1) acc xs
+          where c = head $ drop (r * i) (concat $ repeat x)
+
+slopes :: [(Int, Int)]
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1)]
+
+part2 :: [String] -> Int
+part2 strl = (*) n $ product $ map (\(x, y) -> slopeChecker x y strl) slopes
+  where n = go 0 0 strl
+        go !i acc [] = acc
+        go !i acc [_] = acc
+        go !i acc (x:_:xs) = if (c == '#')
+                             then go (i + 1) (acc + 1) xs
+                             else go (i + 1) acc xs
+          where c = head $ drop i (concat $ repeat x)
