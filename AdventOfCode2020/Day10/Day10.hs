af2133ad7734a08ffc64939328d6026eeb19ee6c
Zod1s <lorefrance15@gmail.com>
2020-12-10 23:42:26 +0100

    Day 10 Part 1

--- AdventOfCode2020/Day10/Day10.hs
+++ AdventOfCode2020/Day10/Day10.hs
@@ -7,9 +7,9 @@ main = do
   print $ part1 input
 
 part1 :: [Int] -> Int
-part1 list = (l1 + 1) * l3
+part1 list = l1 * l3
   where m = maximum list
-        list' = (sort list) ++ [m+3]
+        list' = 0 : (sort list) ++ [m+3]
         toCount = zipWith (-) (tail list') list'
         l1 = length $ filter (==1) toCount
         l3 = length $ filter (==3) toCount
